9750c65584a4d881f7d636f060d6fe50c5cfd638
Graza <code@graza.io>
2024-11-27 15:16:09 +0000

    refactor: mappers return single item (#6)

--- mappers/audit_log_mapper.go
+++ mappers/audit_log_mapper.go
@@ -20,7 +20,7 @@ func (c *AuditLogMapper) Identifier() string {
 	return "audit_log_mapper"
 }
 
-func (c *AuditLogMapper) Map(ctx context.Context, a any) ([]*rows.AuditLog, error) {
+func (c *AuditLogMapper) Map(ctx context.Context, a any) (*rows.AuditLog, error) {
 	// validate input type is string
 	input, ok := a.(string)
 	if !ok {
@@ -37,5 +37,5 @@ func (c *AuditLogMapper) Map(ctx context.Context, a any) ([]*rows.AuditLog, erro
 	row := rows.NewAuditLog()
 	row.FromMap(fields)
 
-	return []*rows.AuditLog{row}, nil
+	return row, nil
 }
